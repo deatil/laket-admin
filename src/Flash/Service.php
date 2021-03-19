c13bceb3daab24fe4efa0ecfd688d5d6ad4b7434
deatil <2217957370@qq.com>
2021-03-19 14:35:25 +0800

    添加闪存扩展服务

---
+++ src/Flash/Service.php
@@ -0,0 +1,60 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Laket\Admin\Flash;
+
+use Laket\Admin\Support\Service as BaseService;
+use Larke\Admin\Traits\FlashService as FlashServiceTrait;
+
+/**
+ * 闪存服务
+ *
+ * @create 2021-3-19
+ * @author deatil
+ */
+class Service extends BaseService
+{
+    use FlashServiceTrait;
+    
+    /**
+     * 闪存composer文件
+     */
+    public $composer = '';
+    
+    /**
+     * 启动，只有启用后加载
+     */
+    public function start()
+    {}
+    
+    /**
+     * 安装后
+     */
+    public function install()
+    {}
+    
+    /**
+     * 卸载后
+     */
+    public function uninstall()
+    {}
+    
+    /**
+     * 更新后
+     */
+    public function upgrade()
+    {}
+    
+    /**
+     * 启用后
+     */
+    public function enable()
+    {}
+    
+    /**
+     * 禁用后
+     */
+    public function disable()
+    {}
+}
