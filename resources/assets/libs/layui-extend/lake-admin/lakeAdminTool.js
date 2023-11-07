f43aa1b5b8036df394678fd501a4238b90ca55ce
deatil <2217957370@qq.com>
2023-11-07 11:56:09 +0800

    修复左侧切换失效问题

--- resources/assets/libs/layui-extend/lake-admin/lakeAdminTool.js
+++ resources/assets/libs/layui-extend/lake-admin/lakeAdminTool.js
@@ -1,5 +1,5 @@
 /*!
- * lakeAdminTool.js v1.0.2
+ * lakeAdminTool.js v1.0.3
  * https://github.com/deatil/lake-admin
  * 
  * Apache License 2.0 © Deatil
@@ -15,7 +15,7 @@ layui.define(['jquery', 'jqueryCookie', 'lakeAdminMenu'], function (exports) {
             $("#side_menus_bar").find(".layui-this").removeClass('layui-this');
             $("#side_menus_bar").find("a[lay-id=" + data_id + "]").parent().addClass('layui-this');
             
-            if ($("#side_menus_bar.lake-admin-module").find("a[lay-id=" + data_id + "]").length > 0) {
+            if ($("#side_menus_bar").find("a[lay-id=" + data_id + "]").length > 0) {
                 var thiz = $("#side_menus_bar").find("a[lay-id=" + data_id + "]");
                 
                 var childDds = $("#side_menus_bar")
@@ -33,20 +33,16 @@ layui.define(['jquery', 'jqueryCookie', 'lakeAdminMenu'], function (exports) {
                 var navChildItems = thiz
                     .parents('#side_menus_bar .layui-nav-item-active');
                 
-                /*
                 $("#side_menus_bar")
                     .find(".layui-nav-item")
                     .removeClass('layui-nav-itemed');
-                */
                 $(navItems).each(function() {
                     $(this).addClass('layui-nav-itemed');
                 });
                 
-                /*
                 $("#side_menus_bar")
                     .find(".layui-nav-item-active")
                     .removeClass('layui-nav-itemed');
-                */
                 $(navChildItems).each(function() {
                     $(this).addClass('layui-nav-itemed');
                 });
