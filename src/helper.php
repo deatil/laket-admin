beb240ec8eaa502c73d36ac79821073675b3b246
deatil <2217957370@qq.com>
2024-07-14 12:32:11 +0800

    更新

--- src/helper.php
+++ src/helper.php
@@ -13,8 +13,8 @@ use Laket\Admin\Model\Flash as FlashModel;
 use Laket\Admin\Model\Attachment as AttachmentModel;
 
 // 版本号
-define("LAKET_VERSION", "1.3.1");
-define("LAKET_RELEASE", "1.3.1.20240713");
+define("LAKET_VERSION", "1.3.11");
+define("LAKET_RELEASE", "1.3.11.20240713");
 
 if (! function_exists('make')) {
     /**
